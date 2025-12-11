198f746dd4effa1919caa62b23287a9ddb27f88c
Diego Andres Eiman <daeiman@gmail.com>
2025-12-11 11:03:26 -0300

    Mejorar ancho del desplegable de empresas para evitar opciones en dos líneas

--- src/components/common/CompanySelector.tsx
+++ src/components/common/CompanySelector.tsx
@@ -36,7 +36,7 @@ export function CompanySelector({
         placeholder="Seleccionar empresa"
         className=""
         useThemeColors={true}
-        style={{ width: 'fit-content', minWidth: '200px', maxWidth: '400px' }}
+        style={{ width: 'fit-content', minWidth: '250px', maxWidth: '600px' }}
       />
     </div>
   );
